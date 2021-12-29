8c520245067ade9f796a3938ffac7b2b6cb451c5
Evanzhao08 <15309299853@189.cn>
2021-12-29 12:32:48 +0800

    引入antd和 Route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+/* 
+入口文件
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'antd/dist/antd.css'
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//App组件标签渲染到index页面div
+ReactDOM.render(<App/>,document.getElementById('root'))
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+/* 后台管理的路由组件 */
+
+export default class admin extends Component {
+    render() {
+        return (
+            <div>
+                Admin
+            </div>
+        )
+    }
+}
